e4ce5eb34aa48dc9ce515ff7693301429005aeab
Dhaval Trivedi <dhaval6244@gmail.com>
2021-02-15 14:56:34 +0530

    slow down app server

--- class-1/hello-world/index.js
+++ class-1/hello-world/index.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const app = express();
+const fs = require("fs");
 const port = 3000;
+const maxIterations = 1000;
+let currentRequestId = 1;
 
 app.get("/", (req, res) => {
+  console.log(`processing request - ${currentRequestId}`);
+  currentRequestId += 1;
+  for (let i = 0; i < maxIterations; i++) {
+    fs.writeFileSync("/tmp/dummy.log", `#{${i}}`);
+  }
   res.send("Hello World!");
 });
 
