fbfb3e5613aed95ff1f34ebdd8aec10e95b4d443
Dhaval Trivedi <dhaval6244@gmail.com>
2021-03-18 11:35:34 +0530

    temp - add redis

--- class-1/hello-world/index.js
+++ class-1/hello-world/index.js
@@ -7,6 +7,13 @@ const maxIterations = 1000;
 let currentRequestId = 1;
 const MongoClient = require("mongodb").MongoClient;
 const url = "mongodb://139.59.24.154:27017/";
+const Redis = require("ioredis");
+const redisClient = new Redis(
+  "rediss://default:iqh4dpoq463wxy9e@db-redis-blr1-12653-do-user-8704168-0.b.db.ondigitalocean.com:25061"
+);
+
+// redisClient.auth("iqh4dpoq463wxy9e");
+
 let mongoConnection;
 
 app.get("/", (req, res) => {
@@ -21,6 +28,8 @@ app.get("/", (req, res) => {
 app.get("/test", async (req, res) => {
   console.log(`processing request - ${currentRequestId}`);
   currentRequestId += 1;
+  // random redis call
+  await redisClient.set("foo", new Date());
 
   // Random mongo call
   const dbo = mongoConnection.db("mydb");
@@ -29,7 +38,12 @@ app.get("/test", async (req, res) => {
   const result = dbo.collection("customers").find(query);
   console.log(await result.toArray());
   // mongo call end
-  res.send("Hello test!");
+
+  // another random redis call
+  const foo = await redisClient.get("foo");
+
+  console.log(foo);
+  res.send(`Hello test!, ${foo}`);
 });
 
 app.listen(port, () => {
